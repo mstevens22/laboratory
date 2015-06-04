87f41ee8d5e76827140cacb9ca9e3ae5a5ec5b5f
Mathias STEVENS <mathiasstevens@gmail.com>
2015-06-04 17:52:56 +0200

    Add server

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('/app'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World of Fame!');
+});
+
+var server = app.listen(80, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
